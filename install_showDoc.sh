01f005535cfbac371a17ae2f7cb3cd45c4f1e806
qq3331053 <825978660@qq.com>
2018-10-27 12:33:37 +0800

    showDoc

---
+++ install_showDoc.sh
@@ -0,0 +1,12 @@
+wget https://www.showdoc.cc/script/showdoc;chmod +x ./showdoc;
+#localhost:4999
+#停止
+#./showdoc stop 
+#重启
+#./showdoc restart
+#升级showdoc到最新版
+#./showdoc update
+#卸载showdoc
+#./showdoc uninstall
+#showdoc/123456
+
